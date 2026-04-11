2a564cf80872cc32198ad6f73762a22b4a498d4d
GemzH11 <GemzH11@gmail.com>
2026-04-11 21:47:24 +0100

    Responding to review comments round 2

--- backend/src/test/java/uk/co/hogandhivecrafts/backend/integration/FlywayIT.java
+++ backend/src/test/java/uk/co/hogandhivecrafts/backend/integration/FlywayIT.java
@@ -2,38 +2,30 @@ package uk.co.hogandhivecrafts.backend.integration;
 
 import org.assertj.core.api.Assertions;
 import org.flywaydb.core.Flyway;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
-import javax.sql.DataSource;
-
 @ActiveProfiles("integration")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FlywayIT {
 
     @Autowired
-    DataSource dataSource;
+    Flyway flyway;
 
     @Autowired
     JdbcTemplate jdbcTemplate;
 
     private final String SELECT_FLYWAY_HISTORY_SUCCESS_COUNT = "SELECT count(*) FROM flyway_schema_history WHERE success = true";
 
-    @BeforeEach
-    void setUp() {
-        // Ensure a fresh database state for the tests
-        Flyway flyway = Flyway.configure().dataSource(dataSource).cleanDisabled(false).load();
-        flyway.clean();
-        flyway.migrate();
-    }
-
     @Test
     void migrationsApplied() {
-        Integer applied = jdbcTemplate.queryForObject(SELECT_FLYWAY_HISTORY_SUCCESS_COUNT, Integer.class);
-        Assertions.assertThat(applied).isGreaterThan(0);
+        Integer appliedMigrations = flyway.info().applied().length;
+        Integer appliedRowCount = jdbcTemplate.queryForObject(SELECT_FLYWAY_HISTORY_SUCCESS_COUNT, Integer.class);
+        Assertions.assertThat(appliedMigrations).isGreaterThan(0);
+        Assertions.assertThat(appliedRowCount).isGreaterThan(0);
+        Assertions.assertThat(appliedMigrations).isEqualTo(appliedRowCount);
     }
 }
